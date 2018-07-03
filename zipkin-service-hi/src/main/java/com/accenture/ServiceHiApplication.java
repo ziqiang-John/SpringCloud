5f9846b6fc80a4403b322a3ada0f699e31beaf0b
ziqiang.a.huang <haimsn@126.com>
2018-07-03 16:30:42 +0800

    add module zipkin,zipkin-service-hi,zipkin-serice-miya

---
+++ zipkin-service-hi/src/main/java/com/accenture/ServiceHiApplication.java
@@ -0,0 +1,43 @@
+package com.accenture;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+
+@SpringBootApplication
+@RestController
+public class ServiceHiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHiApplication.class, args);
+    }
+
+    private static final Logger LOG = Logger.getLogger(ServiceHiApplication.class.getName());
+
+
+    @RequestMapping("/getInfo")
+    public String info(){
+        LOG.log(Level.INFO, "calling trace service-hi");
+        return "hi i'm service-hi!";
+    }
+    @RequestMapping("/hi")
+    public String callHome(){
+        LOG.log(Level.INFO, "calling trace service-hi  ");
+        return restTemplate.getForObject("http://localhost:8989/miya", String.class);
+    }
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+}
