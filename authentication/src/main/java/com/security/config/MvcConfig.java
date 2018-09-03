d1acafe8b1f86f2c333e2a12d6f7e26f28a992a6
ziqiang.a.huang <haimsn@126.com>
2018-09-03 19:03:03 +0800

    add SSO Part.

---
+++ authentication/src/main/java/com/security/config/MvcConfig.java
@@ -0,0 +1,17 @@
+//package com.security.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+//
+//@Configuration
+//public class MvcConfig extends WebMvcConfigurerAdapter{
+//    @Override
+//    public void addViewControllers(ViewControllerRegistry viewControllerRegistry){
+//
+//        viewControllerRegistry.addViewController("/home").setViewName("home");
+//        viewControllerRegistry.addViewController("/").setViewName("home");
+//        viewControllerRegistry.addViewController("/hello").setViewName("hello");
+//        viewControllerRegistry.addViewController("/login").setViewName("login");
+//    }
+//}
---
+++ authentication/src/main/java/com/security/config/SecurityConfig.java
@@ -0,0 +1,62 @@
+package com.security.config;
+
+
+/**
+ * Created by Administrator on 2017/12/16 0016.
+ */
+import com.security.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+//@EnableWebSecurity
+@Order(1)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    UserDetailsService customUserService(){
+        return new CustomUserDetailsService();
+    }
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .anyRequest().authenticated()//所有的请求需要认证即登陆后才能访问
+//                .and()
+//                .formLogin().loginPage("/login")
+//                .failureUrl("/login?error")
+//                .permitAll() //登录页面可任意访问
+//                .and()
+//                .logout().permitAll();//注销请求可任意访问
+//         }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception { // @formatter:off
+        http.requestMatchers()
+                .antMatchers("/login", "/oauth/authorize")
+                .and()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .permitAll();
+    } // @formatter:on
+    @Override
+    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception{
+        authenticationManagerBuilder.userDetailsService(customUserService());
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+
+}
