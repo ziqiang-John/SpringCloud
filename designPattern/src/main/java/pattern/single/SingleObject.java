2cd021005ad4a156930433875545095a118242d0
ziqiang.a.huang <haimsn@126.com>
2019-01-04 18:44:22 +0800

    1.add single pattern.

---
+++ designPattern/src/main/java/pattern/single/SingleObject.java
@@ -0,0 +1,17 @@
+package pattern.single;
+
+public class SingleObject {
+    private static SingleObject instance = new SingleObject();
+
+    //让构造函数为 private，这样该类就不会被实例化
+    private SingleObject(){}
+
+    //获取唯一可用的对象
+    public static SingleObject getInstance(){
+        return instance;
+    }
+
+    public void showMessage(){
+        System.out.println("Hello World!");
+    }
+}
