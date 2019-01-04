c80e2789b92801c9d9846c434a532c8c70853b7a
ziqiang.a.huang <haimsn@126.com>
2019-01-04 18:36:28 +0800

    1.add study module design pattern. 2.add factory pattern and abstract factory pattern.

---
+++ designPattern/src/main/java/pattern/abstractFactory/Square.java
@@ -0,0 +1,9 @@
+package pattern.abstractFactory;
+
+public class Square implements Shape {
+
+    @Override
+    public void draw() {
+        System.out.println("Inside Square::draw() method.");
+    }
+}
---
+++ designPattern/src/main/java/pattern/factory/Shape.java
@@ -0,0 +1,5 @@
+package pattern.factory;
+
+public interface Shape {
+    void draw();
+}
